cab2efbf6debf78d62bfccec30c00c5647cf0b01
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2023-01-02 18:38:03 -0300

    feat: add social media links

--- src/components/section/Navbar.tsx
+++ src/components/section/Navbar.tsx
@@ -8,14 +8,14 @@ export function Navbar() {
       <Avatar src="https://avatars.githubusercontent.com/u/37001732?v=4" />
 
       <ul className="flex flex-row space-x-6">
-        <Link href={''}>
+        <Link href={'/api/social/twitter'}>
           <li className="flex flex-row items-center gap-2 text-white">
             <TwitterLogo weight="fill" aria-hidden />
             <span>Twitter</span>
             <ArrowSquareOut />
           </li>
         </Link>
-        <Link href={''}>
+        <Link href={'/api/social/github'}>
           <li className="flex flex-row items-center gap-2 text-white">
             <GithubLogo weight="fill" aria-hidden />
             <span>Github</span>
