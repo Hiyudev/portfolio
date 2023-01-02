c312191b3de929ed94db923cf8adff4eea56c7b5
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2023-01-02 20:59:32 -0300

    fix: image resizing on small screens

--- src/components/common/Avatar.tsx
+++ src/components/common/Avatar.tsx
@@ -6,7 +6,7 @@ type AvatarProps = {
 
 export default function Avatar({ src }: AvatarProps) {
   return (
-    <div className="flex h-12 w-12 flex-shrink overflow-hidden rounded-full border border-white">
+    <div className="flex h-12 w-12 flex-shrink-0 overflow-hidden rounded-full border border-white">
       <Image src={src} alt="Kevin's profile" width={48} height={48} />
     </div>
   );
