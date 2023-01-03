2909dafc16d628952aaefe97c6af4e0b502e3ec6
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2023-01-02 21:06:54 -0300

    Revert "feat: remove blog posts"
    This reverts commit 9fa9ee1fa9b18464fe6e408669cd67d3a1b18e60.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,6 +69,28 @@ const HomePage: NextPage = ({
               );
             })}
         </ul>
+        <HeadlineComponent
+          className="mt-8 mb-8"
+          id="posts"
+          weight={3}
+          ref={blogHeadline}
+        >
+          Posts
+        </HeadlineComponent>
+        <ul className="flex flex-col gap-4">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => {
+              return (
+                <ProjectCard
+                  key={index}
+                  tags={['React', 'Next.js', 'TypeScript']}
+                  title="Project"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nisl vel ultricies lacinia, nisl nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl."
+                />
+              );
+            })}
+        </ul>
       </div>
     </div>
   );
